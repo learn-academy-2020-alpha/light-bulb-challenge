2ca77c356380146f4c3f743df7f538d7ee1911c8
Rachael Stanislaw <rachaelstanislaw@gmail.com>
2020-02-12 10:58:47 -0800

    README for light bulb challenge

---
+++ lightbulb-challenge/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Your code goes here :)</h1>
+      </div>
+    );
+  }
+}
